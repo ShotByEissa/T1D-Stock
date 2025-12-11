b2e0a4a13a9d22d2fa438d7978f13212b57f5360
Eissa Ahmad <eissa.ahmad31@hotmail.com>
2025-12-11 13:49:20 -0500

    Initial Commit

---
+++ T1D Stock/T1D_StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  T1D_StockApp.swift
+//  T1D Stock
+//
+//  Created by Eissa Ahmad on 2025-12-11.
+//
+
+import SwiftUI
+
+@main
+struct T1D_StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ T1D StockTests/T1D_StockTests.swift
@@ -0,0 +1,17 @@
+//
+//  T1D_StockTests.swift
+//  T1D StockTests
+//
+//  Created by Eissa Ahmad on 2025-12-11.
+//
+
+import Testing
+@testable import T1D_Stock
+
+struct T1D_StockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
